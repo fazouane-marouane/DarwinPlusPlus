fac5a44f14278608a9f2ddc83b8b5b0013f5c6d0
Khalife <khalife.sammy@gmail.com>
2015-06-28 21:44:30 +0200

    modifications

--- include/ProbabilisticEvolutionaryConfig.h
+++ include/ProbabilisticEvolutionaryConfig.h
@@ -27,21 +27,21 @@ namespace Darwin
 			// multinomial by default
 			// other methods: Tournament, SCX
 			// select over a population container
-		    std::random_device rd;
+			std::random_device rd;
 			std::mt19937 gen(rd());
 			Darwin::Rand::uniform_distribution<int> dis_size(int(population.size()/3),int((2/3)*population.size()));
 			N = dis_size(gen);
 			Darwin::Rand::uniform_distribution<int> dis(1,N);
-	        auto rand = [&]() { return dis(gen); };
-	        std::vector<int> Values;
+			auto rand = [&]() { return dis(gen); };
+			std::vector<int> Values;
 
-	        while ( Values.size() < N )
-	        {
-	        	Values.push_back(rand());
-	        	sort( Values.begin(), Values.end() );
-                Values.erase( unique( Values.begin(), Values.end() ), Values.end() );
-	        }
-	        base::individuals_references list_individuals;
+			while ( Values.size() < N )
+			{
+				Values.push_back(rand());
+				sort( Values.begin(), Values.end() );
+			    Values.erase( unique( Values.begin(), Values.end() ), Values.end() );
+			}
+			base::individuals_references list_individuals;
 			std::copy( Values.begin(), Values.end(), std::back_inserter( list_individuals ) );  
 			return list_individuals;          
 		}
