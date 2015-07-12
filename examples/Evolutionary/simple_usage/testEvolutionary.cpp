51748775b7f9999fd489bd1812af6810e041d788
khalife <khalife.sammy@gmail.com>
2015-07-12 15:59:12 +0200

    TSP

--- examples/Evolutionary/simple_usage/testEvolutionary.cpp
+++ examples/Evolutionary/simple_usage/testEvolutionary.cpp
@@ -6,9 +6,50 @@
 #include <ProbabilisticEvolutionaryConfig.h>
 #include <GeneticAlgorithms.h>
 #include <eigen3/Eigen/Dense>
+#include <Random/discrete_distribution.h>
 
 using namespace Eigen;
-using Individual = std::vector<bool>;
+using Individual = std::vector<int>;
+
+namespace Darwin
+{
+	namespace Rand
+	{
+		template<>
+		class uniform_distribution<std::vector<int>>
+		{
+		public:
+			uniform_distribution(size_t _size, std::vector<double> probabilities): size(_size), dists(make_distribution(probabilities))
+			{}
+			template<class Generator>
+			std::vector<int> operator()(Generator& gen)
+			{
+				std::vector<int> ret;
+				ret.reserve(size);
+				for(size_t position = 0; position < size; ++position)
+				{
+					ret.push_back(dists(gen));
+				}
+				return ret;
+			}
+		private:
+			discrete_distribution<double> dists;
+			size_t const size;
+		};
+	}
+}
+
+std::ostream& operator<<(std::ostream& os, const std::vector<int>& vec)
+{
+    os << '[ ';
+    for (auto& el : vec)
+    {
+        os << el << ' ';
+    }
+    os << ' ]';
+    return os;
+}
+
 
 template<class GoalFunction>
 class testEvolutionaryConfig : public Darwin::ProbabilisticEvolutionaryConfig<GoalFunction, Individual, std::vector<Individual>>
@@ -16,7 +57,7 @@ class testEvolutionaryConfig : public Darwin::ProbabilisticEvolutionaryConfig<Go
 	using base = Darwin::ProbabilisticEvolutionaryConfig<GoalFunction, Individual, std::vector<Individual>>;
 public :
 	using base::base;
-	testEvolutionaryConfig(GoalFunction goal, size_t Population_size, size_t dimension): base(goal, Population_size, Darwin::Rand::uniform_distribution<Individual>(dimension)), mutation_dist(0, dimension-1)
+	testEvolutionaryConfig(GoalFunction goal, size_t Population_size, size_t dimension, std::vector<double> probabilities): base(goal, Population_size, Darwin::Rand::uniform_distribution<Individual>(dimension, probabilities)), mutation_dist(0, dimension-1)
 	{
 	}
 
@@ -40,10 +81,16 @@ public :
 
 	Individual crossOver(Individual const& lhs, Individual const& rhs)
 	{
-		if(dist(gen))
-			return lhs & rhs;
-		else
-			return lhs | rhs;
+		if (lhs.size() != rhs.size())
+			throw std::runtime_error("Two population do not have same size");
+		std::vector<int> v(lhs.size());
+		for (int i = 0; i < lhs.size() ; i++){
+			if (i%2 == 0) 
+				v[i] = lhs[i];
+			else 
+				v[i] = rhs[i];
+		}
+		return v;
 	}
 
 	virtual std::vector<Individual> mutate(individuals_references const & mutants)
@@ -56,11 +103,19 @@ public :
 
 	Individual mutate(Individual m)
 	{
-		auto pos = mutation_dist(gen);
-		m[pos] = !m[pos];
+		Darwin::Rand::uniform_distribution<int> dist(1,m.size()-1);
+		int permutation_index = dist(gen);
+		if (permutation_index != m.size()-1){
+			m[permutation_index] = m[permutation_index + 1];
+			m[permutation_index + 1] = m[permutation_index]; 
+		}
+		else{
+			m[permutation_index] = m[permutation_index-1];
+			m[permutation_index-1] = m[permutation_index];
+		}
 		return m;
 	}
-
+    
 	void print()
 	{
 		for (auto individual : population)
@@ -70,7 +125,7 @@ public :
 
 	virtual bool goalReached()
 	{
-		return counter++ > 10;
+		return counter++ > 50;
 	}
 
 	void printBest() const
@@ -88,52 +143,66 @@ public :
 };
 
 template<class GoalFunction>
-testEvolutionaryConfig<GoalFunction> make_testEvolutionaryConfig(GoalFunction goal, size_t size, size_t dimension)
+testEvolutionaryConfig<GoalFunction> make_testEvolutionaryConfig(GoalFunction goal, size_t size, size_t dimension, std::vector<double> probabilities)
 {
-	return testEvolutionaryConfig<GoalFunction>(goal, size, dimension);
+	return testEvolutionaryConfig<GoalFunction>(goal, size, dimension, probabilities);
 }
+
+
+
 
 
 int main()
 {
-	int nbCities = 50;
+	int nbCities = 9;
 	MatrixXf cities(2,nbCities);
 	for (int i = 0; i < nbCities; i++)
 	{
-		cities(1,i) = i%5;
-		cities(2,i) = i/5; 
+		cities(0,i) = i%5;
+		cities(1,i) = i/5;
 	}
 
-	// MatrixXf cityMap(nbCities, nbCities);
-	// for (int i = 0; i < nbCities; i++)
-	// 	for (int j = 0; j < nbCities; j++)
-	// 		cityMap(i,j)= sqrt(i^2 + j^2);
+	MatrixXf cityMap(nbCities, nbCities);
+	for (int i = 0; i < nbCities; i++)
+		for (int j = 0; j < nbCities; j++)
+			cityMap(i,j)= sqrt(i^2 + j^2);
 
-	auto goalFunction = [](std::vector<int> individual)
+	auto goalFunction = [&cityMap](std::vector<int> individual)
 	{
-		// float s = 0;
-		// for (int i = 0; i < individual.size()-1; i++)
-		// {
-		// 	//s += cityMap(individual[i],individual[i+1]); 
-		// 	s -= sqrt((individual[i]/5-individual[i+1]/5)^2 + ((individual[i]%5)-individual[i+1]%5)^2);
-		// 	for (int j = 0; j < individual.size()-1; j++)
-		// 	{
-		// 		s -= ((individual[i] == individual[j]) ? 100 : 0);
-		// 	}
-		// }
-		// return s;
 		float s = 0;
-		for (auto v : individual){
-			s += v;
+		for (int i = 0; i < individual.size()-1; i++)
+		{
+			s -= cityMap(individual.at(i) % cityMap.rows(),individual.at(i+1) % cityMap.rows());
+			//s -= sqrt((individual[i]/5-individual[i+1]/5)^2 + ((individual[i]%5)-individual[i+1]%5)^2);
+			for (int j = 0; j < individual.size()-1; j++)
+			{
+				s -= ((individual[i] == individual[j]) ? 100 : 0);
+			}
 		}
 		return s;
 	};
 
-	// Get result of maximisation
+	// // Get result of maximisation
 
-	auto config = make_testEvolutionaryConfig(goalFunction, 100, nbCities*nbCities);
+	// auto config = make_testEvolutionaryConfig(goalFunction, 100, nbCities*nbCities);
+	// Darwin::GeneticAlgorithmLoop(config);
+	// //config.printBest();
+
+	// auto goalFunction = [](std::vector<int> individual)
+	// {
+	// 	float s = 0;
+	// 	for(auto v: individual)
+	// 	{
+	// 		s += v;
+	// 	}
+	// 	return s;
+	// };
+	std::vector<double> probabilities;
+	for (int i = 0; i < nbCities; i++)
+		probabilities.push_back(1/double(nbCities));
+	auto config = make_testEvolutionaryConfig(goalFunction, 20, nbCities, probabilities);
 	Darwin::GeneticAlgorithmLoop(config);
-	//config.printBest();
-
+    config.print();
+	config.printBest();
     return 0;
 }
